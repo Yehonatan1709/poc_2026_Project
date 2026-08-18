02c94f616dea5c9dc7469ea40f6c01ccacec692a
nof882 <babyoda881@gmail.com>
2026-08-18 18:28:02 +0300

    done

--- POCBEATUIFAL.py
+++ POCBEATUIFAL.py
@@ -1,10 +1,20 @@
+import requests
 from bs4 import BeautifulSoup
 
+#target URL
+url = "https://github.com"
 
-def main():
-    pass hi
+# Fetch the web page content
+response = requests.get(url)
+
+if response.status_code == 200:
+    # parse the HTML
+    soup = BeautifulSoup(response.text, "html.parser")
+    # Extract and print the title
+    print("Page Title:", soup.title.string)
     
-    
-    
-if __name__ == "__main__":
-    main()
+    # Find and print all links on the domain
+    for link in soup.find_all("a"):
+        print(link.get("href"))
+else:
+    print("Failed to retrieve the web page.")
